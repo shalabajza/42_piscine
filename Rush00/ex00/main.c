06a1c761f3c8008d571af2dc1ce1410b76521c9b
Danilo Marinkovic <dmarinko@c2r10s3.42prague.com>
2023-06-12 16:54:21 +0200

    rush fix

--- Rush00/ex00/main.c
+++ Rush00/ex00/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psimcak <psimcak@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dmarinko <dmarinko@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/10 15:35:41 by dmarinko          #+#    #+#             */
-/*   Updated: 2023/06/11 17:31:16 by psimcak          ###   ########.fr       */
+/*   Updated: 2023/06/12 15:43:53 by dmarinko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,43 @@ void	rush02(int x, int y);
 void	rush03(int x, int y);
 void	rush04(int x, int y);
 
-int	main(void)
+int	string_to_int(char *str)
+{
+	int	result;
+	int	i;
+	int	digit;
+
+	result = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		digit = str[i] - '0';
+		result = result * 10 + digit;
+		i++;
+	}
+	return (result);
+}
+
+int	main(int argc, char *argv[])
 {
 	int	num1;
 	int	num2;
 
-	num1 = 1;
-	num2 = 5;
-	// rush00(num1, num2);
+	if (argc == 1)
+	{
+		num1 = 7;
+		num2 = 5;
+	}
+	else if (argc == 2)
+	{
+		num1 = string_to_int(argv[1]);
+		num2 = num1;
+	}
+	else
+	{
+		num1 = string_to_int(argv[1]);
+		num2 = string_to_int(argv[2]);
+	}
 	rush01(num1, num2);
-	// rush02(num1, num2);
-	// rush03(num1, num2);
-	// rush04(num1, num2);
 	return (0);
 }
