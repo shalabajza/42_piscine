50bd7065fff2a92fd4de10df909fb9020f8570d2
Danilo Marinkovic <dmarinko@c1r1s7.42prague.com>
2023-06-15 16:25:58 +0200

    further fixing

--- 04_C02/ex11/ft_putstr_non_printable.c
+++ 04_C02/ex11/ft_putstr_non_printable.c
@@ -6,62 +6,61 @@
 /*   By: dmarinko <dmarinko@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 14:17:25 by dmarinko          #+#    #+#             */
-/*   Updated: 2023/06/14 15:24:03 by dmarinko         ###   ########.fr       */
+/*   Updated: 2023/06/15 16:25:43 by dmarinko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_print_hex(int num)
-{
-	char	c;
-
-	c = '\\';
-	write(1, &c, 1);
-	if (num / 16 < 10)
-		c = (num / 16) + '0';
-	else
-		c = (num / 16) - 10 + 'a';
-	write(1, &c, 1);
-	if (num % 16 < 10)
-		c = (num % 16) + '0';
-	else
-		c = (num % 16) - 10 + 'a';
-	write(1, &c, 1);
-}
-
-int	ft_char_is_printable(char c)
-{
-	if (!((c >= ' ') && (c <= '~')))
-		return (0);
-	return (1);
-}
+#include <stdio.h>
 
 void	ft_putchar(char c)
 {
-	if (ft_char_is_printable(c) == 1)
-		write(1, &c, 1);
-	else
-		ft_print_hex(c);
+	write(1, &c, 1);
 }
 
-void ft_putstr_non_printable(char *str)
+void	ft_put_hexa(int c)
+{
+	char	*base;
+
+	base = "0123456789abcdef";
+	if (c >= 16)
+	{
+		ft_putchar(base[c / 16]);
+		ft_putchar(base[c % 16]);
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(base[c]);
+	}
+}
+
+void	ft_putstr_non_printable(char *str)
 {
 	int	i;
 
-	i = -1;
-	while(str[++i] != '\0')
+	i = 0;
+	while (str[i])
 	{
-		ft_putchar(str[i]);
+		if (str[i] < 32 || str[i] == 127)
+		{
+			ft_putchar('\\');
+			ft_put_hexa(str[i]);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+		}
+		i++;
 	}
 }
 
 /*
 int	main(void)
 {
-	char	string[] = "Coucou\ntu vas bien ?";
+	char	*string = "Coucou\ntu vas bien ?";
 	printf("%s\n\n", string);
-	ft_putstr_non_printable(&string);
+	ft_putstr_non_printable(string);
 	return (0);
 }
 */
