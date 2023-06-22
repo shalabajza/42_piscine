e76e340066aac7167dc6d0cba3a0028dca19c6fb
Danilo Marinkovic <dmarinko@c1r1s2.42prague.com>
2023-06-22 13:47:11 +0200

    update

--- 06_C04/ex02/ft_putnbr.c
+++ 06_C04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: dmarinko <dmarinko@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 13:25:57 by dmarinko          #+#    #+#             */
-/*   Updated: 2023/06/16 11:16:27 by dmarinko         ###   ########.fr       */
+/*   Updated: 2023/06/22 13:40:11 by dmarinko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	rec(int nb)
 {
 	char	c;
 
-	if (!nb)
+	if (nb == 0)
 		return ;
 	rec(nb / 10);
 	c = nb % 10 + '0';
@@ -25,10 +25,20 @@ void	rec(int nb)
 
 void	ft_putnbr(int nb)
 {
+	if (nb == -2147483648)
+	{
+		write (1, "-2147483648", 11);
+		return ;
+	}
 	if (nb < 0)
 	{
 		write(1, "-", 1);
 		nb *= -1;
+	}
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return ;
 	}
 	rec(nb);
 }
