078e941ea365e681b1619942e589607c860b3173
Danilo Marinkovic <dmarinko@c1r2s6.42prague.com>
2023-06-27 22:14:31 +0200

    update

--- 10_C08/ex04/ft_strs_to_tab.c
+++ 10_C08/ex04/ft_strs_to_tab.c
@@ -5,8 +5,72 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmarinko <dmarinko@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/22 14:45:42 by dmarinko          #+#    #+#             */
-/*   Updated: 2023/06/22 14:45:43 by dmarinko         ###   ########.fr       */
+/*   Created: 2023/06/26 18:52:00 by dmarinko          #+#    #+#             */
+/*   Updated: 2023/06/27 21:03:40 by dmarinko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dest = malloc(sizeof(src) * (i + 1));
+	i = 0;
+	if (dest == NULL)
+		return (NULL);
+	else
+	{
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+		return (dest);
+	}
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str		*tab;
+	int				i;
+
+	tab = malloc(sizeof(t_stock_str) * (ac + 1));
+	if (tab == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		tab[i].size = ft_strlen(av[i]);
+		i++;
+	}
+	tab[i].str = 0;
+	return (tab);
+}
--- 10_C08/ex05/ft_show_tab.c
+++ 10_C08/ex05/ft_show_tab.c
@@ -6,7 +6,66 @@
 /*   By: dmarinko <dmarinko@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 14:45:50 by dmarinko          #+#    #+#             */
-/*   Updated: 2023/06/22 14:45:51 by dmarinko         ###   ########.fr       */
+/*   Updated: 2023/06/27 21:02:52 by dmarinko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+	{
+		ft_putchar(n + 48);
+	}
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int		i;
+
+	i = -1;
+	while (par[++i].str != 0)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+	}
+}
